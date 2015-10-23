7680d88d62527437a83e8ce1e2232ecae1f153b6
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2015-10-24 02:16:40 +0900

    Close #2

--- perf.js
+++ perf.js
@@ -9,11 +9,56 @@ _wrap_( function( global ) {
 		global.performance = {};
 	}
 
-	// now
-	var startOffset =  new global.Date().getTime();
+	// navigationStart
+	var navigationStart =  new global.Date().getTime();
+
+	var performanceEntryHash = {};
+	var performanceEntryList = [];
+
+	/**
+	 * The Performance.now() method returns a DOMHighResTimeStamp, measured in milliseconds,
+	 * accurate to one thousandth of a millisecond.
+	 * @name performance.now
+	 * @return {DOMHighResTimeStamp} now
+	 * @see http://www.w3.org/TR/hr-time/#dom-performance-now
+	 */
 	global.performance.now = function() {
-		return new global.Date().getTime() - startOffset;
+		return new global.Date().getTime() - navigationStart;
 	}
+
+	/**
+	 * @name PerformanceEntry
+	 * @property {DOMString} name
+	 * @property {DOMString} entryType - mark, measure, navigation, frame, resource, server
+	 * @property {DOMHighResTimeStamp} startTime
+	 * @property {DOMHighResTimeStamp} duration
+	 * @see http://www.w3.org/TR/performance-timeline/#performanceentry
+	 */
+	function PerformanceEntry( name, entryType, startTime, duration ) {
+		this.name = name;
+		this.entryType = entryType;
+		this.startTime = startTime;
+		this.duration = duration;
+	 }
+
+	/**
+	 * This method stores a timestamp with the associated name (a "mark").
+	 * @name performance.mark
+	 * @param {String} name
+	 * @return {DOMHighResTimeStamp} now
+	 * @see http://www.w3.org/TR/hr-time/#dom-performance-now
+	 */
+	global.performance.mark = function( name ) {
+		var performanceEntry = new PerformanceEntry( name, "mark", global.performance.now(), 0 );
+		performanceEntryList.push( performanceEntry );
+		performanceEntryHash[ name ] = performanceEntry;
+	}
+
+	return {
+		performanceEntryHash: performanceEntryHash,
+		performanceEntryList: performanceEntryList,
+		PerformanceEntry: PerformanceEntry
+	};
 } );
 
 // performance.now();
--- test/performance.test.js
+++ test/performance.test.js
@@ -11,7 +11,7 @@ describe("performance.now", function() {
 	
 	beforeEach(function() {
 		mock.count = 0;
-		keyMap = cache(mock);
+		cache(mock);
 	});
 
 	it("should be now time.", function() {
@@ -26,5 +26,47 @@ describe("performance.now", function() {
 		var expected = mock.performance.now();
 		// Then
 		expect(expected).toBe(2);
+	});
+});
+
+describe("performance.mark", function() {
+	var mock = {
+		count : 0,
+		Date : function(){
+			this.getTime = function(){
+				mock.count++;
+				return mock.count;
+			}
+		}
+	};
+	
+	beforeEach(function() {
+		mock.count = 0;
+		performanceStoreObj = cache(mock);
+	});
+
+	it("should be set performanceEntryList and performanceEntryHash.", function() {
+		// Given
+		// When
+		mock.performance.mark("test");
+		// Then
+		expect(performanceStoreObj.performanceEntryList.length).toBe(1);
+		expect(performanceStoreObj.performanceEntryHash.test.constructor)
+			.toBe(performanceStoreObj.PerformanceEntry);
+
+	});
+
+	it("should be store info of performanceEntry.", function() {
+		// Given
+		var performanceEntry;
+		// When
+		mock.performance.mark("something");
+		performanceEntry = performanceStoreObj.performanceEntryHash.something;
+		// Then
+		expect(performanceEntry.name).toBe("something");
+		expect(performanceEntry.entryType).toBe("mark");
+		expect(performanceEntry.startTime).toBe(1);
+		expect(performanceEntry.duration).toBe(0);
+
 	});
 });
